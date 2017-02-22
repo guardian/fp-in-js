6f8de1199f42249d69831b5057d06466d0b72085
Joseph Smith <joseph.smith@guardian.co.uk>
2017-02-22 12:16:49 +0000

    Add first exercise

---
+++ exercises/rewrite-with-map.js
@@ -0,0 +1,15 @@
+var numbers = [1, 4, 9];
+var roots = [];
+
+for (var i=0; i < numbers.length; i++) {
+  roots.push(Math.sqrt(numbers[i]));
+}
+
+console.log(roots);
+
+var doubledRoots = [];
+for (var i=0; i < roots.length; i++) {
+  doubledRoots.push(roots[i] * 2);
+}
+
+console.log(doubledRoots);
