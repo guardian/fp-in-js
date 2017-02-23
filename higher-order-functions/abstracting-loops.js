e7b8b3e59cb4803d082c06eff413106c67b1c774
Joseph Smith <joseph.smith@guardian.co.uk>
2017-02-23 18:00:30 +0000

    More tabs to spaces

--- higher-order-functions/abstracting-loops.js
+++ higher-order-functions/abstracting-loops.js
@@ -2,10 +2,10 @@ var numbers = [1, 4, 9];
 var roots = [];
 
 for (var i=0; i < numbers.length; i++) {
-  	roots.push(Math.sqrt(numbers[i]));
+    roots.push(Math.sqrt(numbers[i]));
 }
 
 var doubledRoots = [];
 for (var i=0; i < roots.length; i++) {
-  	doubledRoots.push(roots[i] * 2);
+    doubledRoots.push(roots[i] * 2);
 }
