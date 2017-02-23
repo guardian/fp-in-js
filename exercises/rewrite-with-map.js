8fbc1ad4a6abbe4b76bc6419cdcdcf987254dcdf
Joseph Smith <joseph.smith@guardian.co.uk>
2017-02-23 21:26:09 +0000

    Exercises tidy-up

--- exercises/rewrite-with-map.js
+++ exercises/rewrite-with-map.js
@@ -2,14 +2,14 @@ var numbers = [1, 4, 9];
 var roots = [];
 
 for (var i=0; i < numbers.length; i++) {
-  roots.push(Math.sqrt(numbers[i]));
+    roots.push(Math.sqrt(numbers[i]));
 }
-
-console.log(roots);
 
 var doubledRoots = [];
 for (var i=0; i < roots.length; i++) {
-  doubledRoots.push(roots[i] * 2);
+    doubledRoots.push(roots[i] * 2);
 }
 
-console.log(doubledRoots);
+// Rewrite using the .map() method on Array.
+// (Ideally return the final contents of doubledRoots
+// without any intermediate variables)
