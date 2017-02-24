c95bacd1a3880d7f727c63636a4b2584f2a5c766
Joseph Smith <joseph.lochlann@gmail.com>
2017-02-24 13:09:44 +0000

    Update first-class-functions.js

--- higher-order-functions/first-class-functions.js
+++ higher-order-functions/first-class-functions.js
@@ -4,11 +4,9 @@ var obj = {key: 42, func: function() { return 42; }}
 
 var x = function() { return 42; };
 
-var y = function() { return function() { return 42} };
+var y = function() { return function() { return 42 } };
 
 42 + (function() { return 42; })()
-//=> 84
 
 function weirdAdd(n, f) { return n + f() }
 weirdAdd(42, function() { return 42 });
-//=> 84
